3876953ef9fda37c5471eb93cbcd319a4c9b202d
jcarlos8 <80362842+jcarlos8@users.noreply.github.com>
2022-12-16 14:57:39 +0000

    jquery version

--- movie_app/js/app.js
+++ movie_app/js/app.js
@@ -1,5 +1,5 @@
-var searchInput = document.querySelector('.search');
-var itemWrapper = document.querySelector('main');
+var searchInput = $('.search');
+var itemWrapper = $('main');
 
 function displayMatches(matches) {
     itemWrapper.innerHTML = '';
@@ -9,7 +9,7 @@ function displayMatches(matches) {
     }
 
         for (var matchObj of matches) {
-        itemWrapper.insertAdjacentHTML('beforeend', `
+        itemWrapper.append(`
         <div class="movie-item" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${matchObj.Poster})">
         <h3>${matchObj.Title}</h3>
         <p>Release Year: ${matchObj.Year}</p>
@@ -21,25 +21,18 @@ function displayMatches(matches) {
 
 function getMovieData(event) {
     var keyCode = event.keyCode;
-    var searchText = searchInput.value.trim().toLowerCase();
+    var searchText = searchInput.value.trim();
   
     if (keyCode === 13 && searchText) {
-      var responsePromise = fetch(`https://www.omdbapi.com/?apikey=20dc4c7f&s=${searchText}`);
-  
-      function handleResponse(responseObj) {
-        return responseObj.json();
-      }
-  
-      responsePromise
-        .then(handleResponse)
-        .then(function (data) {
-          displayMatches(data.Search);
-        });
+      $.get(`https://www.omdbapi.com/?apikey=20dc4c7f&s=${searchText}`)
+      .then(function (data) {
+        displayMatches(data.Search);
+      });        
     }
   }
   
   function init() {
-    searchInput.addEventListener('keydown', getMovieData);
+    searchInput.keydown(getMovieData);
   }
   
   init();
